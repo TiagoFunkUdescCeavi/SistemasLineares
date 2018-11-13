2c49b8a355802f1eee77fffd7097fa4ddfe131c7
Tiago Funk <tiagoff.tf@gmail.com>
2018-11-13 20:57:46 -0200

    Pronto em Java e C++

--- GaussJava/src/GaussJava.java
+++ GaussJava/src/GaussJava.java
@@ -4,19 +4,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-
-/*
-*   O argumento do método calcular é a matriz que representa o sistema linear à ser resolvido.
-*   Este algoritmo esta pronto, incluive com a troca de linha em caso de elemento da diagonal principal
-*   igual à zero,  
-*   Possue ainda a checagem para ver se o sistema é:
-*   Impossível: após a escalonagem a ultima linha deve possuir apenas elementos iguais a zero com 
-*   excessão da ultima posição. ex.: 0 0 0 6.
-*   Indeterminado: Após a escalonagem a ultima linha possui apenas elementos iguais à zero.
-*   Nestes dois casos é retornado null, ainda não trabalhei em como diferenciar os dois casos.
-*   Caso o sistema seja Determinado é realizado o cálculo das respostas e é retornado o vetor com as respostas.
-*/
-
 public class GaussJava {
     
     public static double[][] ler(String arquivo) throws FileNotFoundException, IOException{
@@ -46,11 +33,9 @@ public class GaussJava {
     public static double[] calcular(double[][] sistema) {
         int n = sistema.length - 1;
         double m, aux;
+        double soma;
 
-        //Inicia o escalonamento da matriz
         for (int i = 0; i < n; i++) {
-
-            //Se algum elemento da diagonal principal é igual a zero é iniciado a troca de linhas.
             if (sistema[i][i] == 0 && i != n) {
                 for (int j = i; j <= n; j++) {
                     if (sistema[j][i] != 0 && j != i) {
@@ -63,41 +48,26 @@ public class GaussJava {
                     }
                 }
             }
-            // Finaliza a troca de linhas.
             
-            //Aonde inicia o escalonamento em si.
             for (int j = i + 1; j <= n; j++) {
-                m = - (sistema[j][i] / sistema[i][i]);
-                for (int k = i; k <= n + 1; k++) {
+                m = -1 * (sistema[j][i] / sistema[i][i]);
+                for (int k = 0; k <= n + 1; k++) {
                     sistema[j][k] += m * sistema[i][k];
                 }
             }
-            ////Aonde finaliza o escalonamento em si.
         }
-        //Finaliza o escalonamento da matriz.
 
-        //inicia a checagem para ver se o sistema é impossível, indeterminado ou determinado.
-        if (sistema[n][n] == 0 && sistema[n][n + 1] == 0) {
-            return null;
-        } else if (sistema[n][n] == 0 && sistema[n][n + 1] != 0) {
-            return null;
-        } else {
-            //Inicia o calculo das respostas.
-            double[] respostas = new double[n + 1];
-            respostas[n] = sistema[n][n + 1] / sistema[n][n];
-            double soma;
-            for (int i = n - 1; i >= 0; i--) {
-                soma = 0;
-                for (int j = i + 1; j <= n; j++) {
-                    soma += sistema[i][j] * respostas[j];
-                    respostas[i] = (sistema[i][j + 1] - soma) / sistema[i][i];
-                }
+        double[] respostas = new double[n + 1];
+        respostas[n] = sistema[n][n + 1] / sistema[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            soma = 0;
+            for (int j = i + 1; j <= n; j++) {
+                soma += sistema[i][j] * respostas[j];
+                respostas[i] = (sistema[i][j + 1] - soma) / sistema[i][i];
             }
-            //Finaliza o cálculo das respostas.
-
-            return respostas;
         }
-        //Finaliza a checagem para ver se o sistema é impossível, indeterminado ou determinado.
+
+        return respostas;
 
     }
     
--- GuassCPP/main.cpp
+++ GuassCPP/main.cpp
@@ -4,19 +4,18 @@ using std::cerr;
 using std::endl;
 using std::ios;
 
-#include <string>
-using std::string;
-
 #include <fstream>
 using std::ifstream;
 
 #include <cstdlib>
 using std::exit;
 
-int lerOrdem(string arquivo){
+#include <time.h>
+
+int lerOrdem(char* arquivo){
     int ordem;
     
-    ifstream arq(arquivo.c_str(), ios::in);
+    ifstream arq( arquivo, ios::in );
     if( !arq ){
         cerr << "Arquivo não pode ser aberto." << endl;
         exit( 1 );
@@ -29,11 +28,11 @@ int lerOrdem(string arquivo){
     return ordem;
 }
 
-double** lerMatriz(string arquivo){
+double** lerMatriz(char* arquivo){
     int ordem;
     double** matriz;
     
-    ifstream arq(arquivo.c_str(),ios::in);
+    ifstream arq( arquivo, ios::in );
     if( !arq ){
         cerr << "Arquivo não pode ser aberto." << endl;
         exit( 1 );
@@ -56,20 +55,76 @@ double** lerMatriz(string arquivo){
     return matriz;
 }
 
+double* calcular(int ordem, double** sistema){
+    int n = ordem - 1;
+    double m, aux, soma;
+    double* respostas;
+    
+    for (int i = 0; i < n; i++) {
+        if (sistema[i][i] == 0 && i != n) {
+            for (int j = i; j <= n; j++) {
+                if (sistema[j][i] != 0 && j != i) {
+                    for (int k = 0; k <= n + 1; k++) {
+                        aux = sistema[i][k];
+                        sistema[i][k] = sistema[j][k];
+                        sistema[j][k] = aux;
+                    }
+                    break;
+                }
+            }
+        }
+
+        for (int j = i + 1; j <= n; j++) {
+            m = -1 * (sistema[j][i] / sistema[i][i]);
+            for (int k = 0; k <= n + 1; k++) {
+                sistema[j][k] += m * sistema[i][k];
+            }
+        }
+    }
+    
+    respostas = (double*) malloc( (n + 1) * sizeof( double ) );
+    respostas[n] = sistema[n][n+1] / sistema[n][n];
+    for (int i = n - 1; i >= 0; i--) {
+        soma = 0;
+        for (int j = i + 1; j <= n; j++) {
+            soma += sistema[i][j] * respostas[j];
+            respostas[i] = (sistema[i][j + 1] - soma) / sistema[i][i];
+        }
+    }
+    
+    return respostas;
+}
+
 int main(int argc, char** argv) {
     int ordem;
-    double** m;
-    const string arquivo = "/home/tiago/Repositorios/Github/SistemasLineares/Instancias/b.txt";
+    double* respostas;
+    double** matriz;
+    clock_t tempoInicialLeitura, tempoFinalLeitura;
+    clock_t tempoInicialCalculo, tempoFinalCalculo;
     
-    ordem = lerOrdem(arquivo);
-    m = lerMatriz(arquivo);
+    ordem = lerOrdem( argv[1] );
+    
+    tempoInicialLeitura = clock();
+    matriz = lerMatriz( argv[1] );
+    tempoFinalLeitura = clock();
+    
+    tempoInicialCalculo = clock();
+    respostas = calcular(ordem, matriz);
+    tempoFinalCalculo = clock();
     
     for (int i = 0; i < ordem; i++) {
-        for (int j = 0; j < ordem+1; j++) {
-            cout << m[i][j] << " ";
-        }
-        cout << endl;
+        cout << respostas[i] << " ";
     }
+    cout << endl;
+    
+    cout.precision(17);
+    double div = (double) CLOCKS_PER_SEC / 1000;
+    
+    cout << "Leitura: " << (tempoFinalLeitura - tempoInicialLeitura) << endl;
+    cout << "Leitura: " << (tempoFinalLeitura - tempoInicialLeitura) / div << endl;
+    
+    cout << "Leitura: " << (tempoFinalCalculo - tempoInicialCalculo) << endl;
+    cout << "Leitura: " << (tempoFinalCalculo - tempoInicialCalculo) / div << endl;
     
     return 0;
 }
